9cb3cc839db3233f40e1f0c9943070a086fb9dc4
Pedro Sousa <psousa@equalexperts.com>
2019-09-13 10:12:04 +0100

    Clear all IrnTables before refresh

--- src/worker/main.ts
+++ src/worker/main.ts
@@ -3,7 +3,7 @@ import { pipe } from "fp-ts/lib/pipeable"
 import { chain, run } from "fp-ts/lib/ReaderTaskEither"
 import { task } from "fp-ts/lib/Task"
 import { fold } from "fp-ts/lib/TaskEither"
-import { buildEnvironment } from "../environment"
+import { buildEnvironment, Environment } from "../environment"
 import { irnCrawler } from "../irnCrawler/main"
 import { globalIrnTables } from "../staticData/irnTables"
 import { ServiceError } from "../utils/audit"
@@ -12,32 +12,38 @@ import { safeConfig } from "../utils/config"
 const exitProcess = (error: ServiceError) => {
   debug("Shutting down Worker", error.message)
   process.exit(1)
+  return task.of(undefined)
+}
+
+const addLocalIrntables = (environment: Environment) => environment.irnRepository.addIrnTables(globalIrnTables)
+
+const refreshTables = (environment: Environment) =>
+  pipe(
+    environment.irnRepository.clearAllTables(),
+    chain(() => irnCrawler.refreshTables({ startDate: new Date(Date.now()) })),
+  )
+
+const start = (environment: Environment) => {
+  debug("Worker Config =====>\n", safeConfig(environment.config))
+  run(
+    pipe(
+      irnCrawler.start(),
+      chain(() => environment.irnRepository.updateConfig({ refreshStarted: new Date(Date.now()) })),
+      chain(() =>
+        environment.config.infra.useLocalIrnTables ? addLocalIrntables(environment) : refreshTables(environment),
+      ),
+      chain(() => environment.irnRepository.updateConfig({ refreshEnded: new Date(Date.now()) })),
+      chain(() => environment.irnRepository.end()),
+    ),
+    environment,
+  )
+  return task.of(undefined)
 }
 
 export const startWorker = async () => {
   const process = pipe(
     buildEnvironment(),
-    fold(
-      e => task.of(exitProcess(e)),
-      environment => {
-        debug("Worker Config =====>\n", safeConfig(environment.config))
-        run(
-          pipe(
-            irnCrawler.start(),
-            chain(() => environment.irnRepository.updateConfig({ refreshStarted: new Date(Date.now()) })),
-            chain(() =>
-              environment.config.infra.useLocalIrnTables
-                ? environment.irnRepository.addIrnTables(globalIrnTables)
-                : irnCrawler.refreshTables({ startDate: new Date(Date.now()) }),
-            ),
-            chain(() => environment.irnRepository.updateConfig({ refreshEnded: new Date(Date.now()) })),
-            chain(() => environment.irnRepository.end()),
-          ),
-          environment,
-        )
-        return task.of(undefined)
-      },
-    ),
+    fold(exitProcess, start),
   )
 
   await process()
