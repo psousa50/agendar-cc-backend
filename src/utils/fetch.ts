0562d4f9b87de4996c1c18dc63c494c946c269ee
Pedro Sousa <psousa@equalexperts.com>
2019-09-14 12:49:59 +0100

    Remove req.query log

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -16,7 +16,7 @@ export type FetchAction = (
 
 export const fetchAction = (input: Request | string, init?: RequestInit): ActionResult<Response> => {
   function coreFetch(): ActionResult<Response> {
-    logDebug("Fetching... =====>", input, init && init.body)
+    logDebug("Fetching... =====>", input)
     return fromTaskEither(tryCatch(() => isoFetch(input, init), error => new ServiceError((error as Error).message)))
   }
 
