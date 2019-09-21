0cc263e26d84f0f71fcf857c4bfd524236df6956
Pedro Sousa <psousa@equalexperts.com>
2019-09-21 12:14:50 +0100

    Add full IrnTables

--- src/t.ts
+++ src/t.ts
@@ -1,40 +1,10 @@
-import { MongoClient } from "mongodb"
+import { sort } from "ramda"
+import { globalCounties } from "./staticData/counties"
 import { globalDistricts } from "./staticData/districts"
 
-const test = async () => {
-  const client = await MongoClient.connect("mongodb://localhost:27017/test", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+const allDistricts = globalCounties.map(c => {
+  const d = globalDistricts.find(dd => dd.districtId === c.districtId)!
+  return `${d.name} - ${c.name}`
+})
 
-  const cols0 = await client.db().collections()
-  console.log(`COLLECTION}=====>\n`, cols0.map(c => c.collectionName))
-
-  await client
-    .db()
-    .collection("TEST1")
-    .insertMany(globalDistricts)
-
-  const cols = await client.db().collections()
-  console.log(`COLLECTION}=====>\n`, cols.map(c => c.collectionName))
-
-  await client
-    .db()
-    .collection("TEST1")
-    .rename("TEST2", { dropTarget: true })
-
-  const d = await client
-    .db()
-    .collection("TEST2")
-    .find({})
-    .toArray()
-
-  console.log("Districts", d.map(c => c.name))
-
-  const cols2 = await client.db().collections()
-  console.log(`COLLECTION}=====>\n`, cols2.map(c => c.collectionName))
-
-  await client.close()
-}
-
-test()
+sort((d1, d2) => d1.localeCompare(d2), allDistricts).map(c => console.log(c))
