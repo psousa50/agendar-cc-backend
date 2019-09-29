b324c86474b6d38710218b015beda0347016c477
Pedro Sousa <psousa@equalexperts.com>
2019-09-29 19:08:18 +0100

    Add IrnPlaces

--- src/t.ts
+++ src/t.ts
@@ -1,6 +1,7 @@
-import { sort } from "ramda"
+import { sort, uniq } from "ramda"
 import { globalCounties } from "./staticData/counties"
 import { globalDistricts } from "./staticData/districts"
+import { globalIrnPlaces } from "./staticData/irnPlaces"
 import { logDebug } from "./utils/debug"
 
 const allDistricts = globalCounties.map(c => {
@@ -9,3 +10,14 @@ const allDistricts = globalCounties.map(c => {
 })
 
 sort((d1, d2) => d1.localeCompare(d2), allDistricts).map(c => logDebug(c))
+
+const uniqD = sort((d1, d2) => d1 - d2, uniq(globalIrnPlaces.map(p => p.districtId)))
+
+console.log("=====>\n", uniqD)
+
+const dWithPlaces = globalDistricts.map(d => ({
+  ...d,
+  countPlaces: globalIrnPlaces.filter(p => p.districtId === d.districtId).length,
+}))
+
+console.log("=====>\n", dWithPlaces)
