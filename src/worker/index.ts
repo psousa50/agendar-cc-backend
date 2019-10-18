f4bdb0161d5a849c6bfa9ab82259c014b9660d73
Pedro Sousa <psousa@equalexperts.com>
2019-10-18 12:58:59 +0100

    Code cleanup

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -5,5 +5,5 @@ import { startWorker } from "./main"
 dotenv.config()
 
 startWorker()
-  .then(() => logDebug("Worker Terminated"), e => logDebug(`Error: ${e.message}`))
+  .then(() => logDebug("Worker Started"), e => logDebug(`Error: ${e.message}`))
   .catch((e: any) => logDebug(`Error: ${e.message}`))
