95e05e895b81dfdb5eff6e61b401e0f7724b577b
Pedro Sousa <pedronsousa@gmail.com>
2020-06-09 14:11:49 +0100

    misc

---
+++ src/t.ts
@@ -0,0 +1,28 @@
+import { either, left, right } from "fp-ts/lib/Either"
+import { pipe } from "fp-ts/lib/pipeable"
+import { chain, fromEither, orElse } from "fp-ts/lib/ReaderTaskEither"
+
+const f = async () => {
+  const t1 = fromEither<string, boolean, void>(left(false))
+  // const t1 = fromEither<string, boolean, number>(right(2))
+
+  const a = either.of(10)
+  const b = pipe(
+    a,
+    chainLeft,
+  )
+
+  const t2 = pipe(
+    t1,
+    chain(_ => fromEither<string, boolean, void>(right(undefined))),
+  )
+
+  const t3 = pipe(
+    t2,
+    orElse<string, boolean, void, boolean>(() => fromEither(right(undefined))),
+  )
+
+  return await t3("env")()
+}
+
+f().then(x => console.log("=====>\n", x))
